77939105ac0dc5e64e31b31769f23be3ba7ffde8
Suraj Vijay <surajcpp115@gmail.com>
2022-04-16 13:29:52 +0530

    neumoprhism for title

--- src/components/title/styles.tsx
+++ src/components/title/styles.tsx
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
 export const TitleText = styled.span<{ dark?: boolean }>`
+  padding: 0% 1% 0.5% 1%;
+  border-radius: 0.42rem;
   font-family: geomanist-regular;
   font-weight: bold;
   font-size: 3.6rem;
   color: rgba(0, 0, 0, 0.91);
-  ${(props) => props.dark && `color: #f5f4f9;`}
+  background-color: #f5f4f9;
+  box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.7);
+  -webkit-box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.7);
+  -moz-box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.7);
+  ${(props) =>
+    props.dark &&
+    `
+    box-shadow: 1px 1px 5px 1px rgba(245, 244, 249, 0.7);
+    -webkit-box-shadow: 1px 1px 5px 1px rgba(245, 244, 249, 0.7);
+    -moz-box-shadow: 1px 1px 5px 1px rgba(245, 244, 249, 0.7);
+    background-color: #171717;
+    color: #f5f4f9;
+  `}
   text-shadow: 
     1px 0px 1px #ccc, 0px 1px 1px #eee, 
     2px 1px 1px #ccc, 1px 2px 1px #eee,
