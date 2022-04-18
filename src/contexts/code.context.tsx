f4fcfe0aab41bb51255a4e776ea453ecec0c68ad
Suraj Vijay <surajcpp115@gmail.com>
2022-04-18 20:58:37 +0530

    added deployed api endpoint

--- src/contexts/code.context.tsx
+++ src/contexts/code.context.tsx
@@ -19,6 +19,10 @@ export const CodeContext = createContext<CodeContextInterface>(defaultState);
 
 export const useCodeContext = () => useContext(CodeContext);
 
+const API_URL = process.env.NODE_ENV === "development" 
+? "http://192.168.29.97:5000"
+: "https://cacg-g0twd6blo-its-me-sv.vercel.app";
+
 export const CodeContextProvider: React.FC<{children: ReactNode}> = ({children}) => {
   const [status, setStatus] = useState<number>(defaultState.status);
   const [code, setCode] = useState<string>(defaultState.code);
@@ -27,24 +31,24 @@ export const CodeContextProvider: React.FC<{children: ReactNode}> = ({children})
 
   const fetchCode = (addend: string, augend: string, sum: string) => {
     setLoading(true);
-    fetch(`http://192.168.29.97:5000/api/generate`, {
+    fetch(`${API_URL}/api/generate`, {
       method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({addend, augend, sum})
+      body: JSON.stringify({ addend, augend, sum }),
     })
-    .then(res => res.json())
-    .then(({status: rs, code: rc, time: rt}) => {
-      setStatus(rs);
-      if (rs === 1) {
-        setCode(rc);
-        setTime(rt);
-      }
-      setLoading(false);
-    })
-    .catch(() => setLoading(false));
+      .then((res) => res.json())
+      .then(({ status: rs, code: rc, time: rt }) => {
+        setStatus(rs);
+        if (rs === 1) {
+          setCode(rc);
+          setTime(rt);
+        }
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
   };
 
   return (
